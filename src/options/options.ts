cd5a1622819732221b032c863e00973a4ba26afc
eunjun park <eunjunpark@eunjunui-MacBookPro.local>
2020-04-22 01:27:47 +0900

    Remove unused files.

--- src/options/options.ts
+++
@@ -1,6 +0,0 @@
-import { browser } from 'webextension-polyfill-ts';
-import './options.scss';
-
-(() => {
-    console.debug('options');
-})();
--- src/popup/popup.ts
+++
@@ -1,6 +0,0 @@
-import { browser } from 'webextension-polyfill-ts';
-import './popup.scss';
-
-(() => {
-    console.debug('popup');
-})();
